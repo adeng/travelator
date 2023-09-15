1f5f3893290f7d9a91d2784bbe845ced7728a726
Albert Deng <albertdeng94@outlook.com>
2023-09-14 22:33:53 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'travelator',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
